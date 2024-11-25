3122191c9def8ef06110f053b84813f41c82b0d8
Maria <mfgriman@gmail.com>
2024-11-25 13:59:38 -0400

    Initial setup of GameOverScene and AboutScene classes.

--- SceneClasses/AboutScene.cs
+++ SceneClasses/AboutScene.cs
@@ -1,12 +1,29 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace GhostHunter
 {
     internal class AboutScene
     {
+        private SpriteFont font;
+        private string aboutSceneText;
+
+        public AboutScene(SpriteFont font)
+        {
+            this.font = font;
+            aboutSceneText = "Description of the game";
+        }
+
+        public void Draw(SpriteBatch spriteBatch) {
+            spriteBatch.Begin();
+            spriteBatch.DrawString(font, aboutSceneText, new Vector2(100, 100), Color.White);
+            spriteBatch.End();
+        }
     }
 }
